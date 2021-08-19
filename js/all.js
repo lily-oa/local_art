423dcc5e34f6e034729bd636e35075ece146cf70
lily <a0976198586@gmail.com>
2021-08-19 20:29:19 +0800

    js update

--- js/all.js
+++ js/all.js
@@ -2,5 +2,9 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 /*---------------------------------------------- */
